9c9b5f8270da99e87bbb95d0853d6ea4004690ab
DP1117 <dpark070111@gmail.com>
2024-03-08 22:00:59 -0500

    controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Arm.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm {
+
+    public Arm(){
+
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Controls.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.stuyfission.fissionlib.input.GamepadStatic.Input;
+
+
+public class Controls {
+
+    public static final Input Arm = Input.LEFT_TRIGGER;
+
+}

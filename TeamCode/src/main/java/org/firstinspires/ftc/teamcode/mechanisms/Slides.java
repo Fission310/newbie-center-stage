59e4f89ce488556859ea0df62da6d91095823d09
TgTuro4 <tgturo4@gmail.com>
2024-03-10 20:09:02 -0400

    Made changes to Slides class, may be errors, misunderstood code has been slashed out, and need some clarification on some lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slides.java
@@ -1,4 +1,107 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-public class Slides {
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.stuyfission.fissionlib.input.GamepadStatic;
+import com.stuyfission.fissionlib.util.Mechanism;
+
+import org.firstinspires.ftc.teamcode.opmode.teleop.Controls;
+import org.firstinspires.ftc.teamcode.util.PIDController;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Slides extends mechanisms{
+
+    public static double LOW_POS = 0;
+    public static double MED_POS = 0;
+    public static double HIGH_POS = 0;
+    public static double LOW_VOLTAGE = 12;
+    public static int ABIT = 140;
+
+    private static double[] POSITIONS = {LOW_POS, MED_POS, HIGH_POS};
+
+    public static double KP = 0.003;
+    public static double KI = 0;
+    public static double KD = 0;
+    public static double MIN_POWER = -0.3;
+    public static double target = 0;
+    public static double power = 0;
+
+    private final PIDController controller = new PIDController(KP, KI, KD);
+
+    private final DcMotorEx[] motors = new DcMotorEx[0];
+    private VoltageSensor voltage;
+
+    public Slides(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        motors[0] = hwMap.get(DcMotorEx.class, "slidesLeftMotor");
+        motors[0] = hwMap.get(DcMotorEx.class, "slidesRightMotor");
+
+        voltage = hwMap.get(VoltageSensor.class, "Control Hub");
+
+        motors[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motors[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motors[0].setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER); // might be wrong RunMode
+        motors[0].setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+        motors[0].setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        motors[0].setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        motors[0].setDirection(DcMotorEx.Direction.REVERSE);
+        motors[0].setDirection(DcMotorEx.Direction.FORWARD);
+
+    }
 }
+
+//    public void telemetry(Telemetry telemetry) {
+//        telemetry.addData("Current Position", getPosition());
+//        telemetry.addData("Target", target);
+//        telemetry.addData("Power", power);
+//        telemetry.update();
+//    }
+
+    public void lowPos() {
+        setTarget(LOW_POS);
+    }
+    public void medPos() {
+        setTarget(MED_POS);
+    }
+    public void highpos() {
+        setTarget(HIGH_POS);
+    }
+
+    public void upABit() {
+        setTarget(target + ABIT);
+    }
+    public void downABit() {
+        setTarget(target - ABIT);
+    }
+
+    public void update() {
+        controller.setTarget(target);
+        power = controller.calculate(getPosition());
+        if (power < MIN_POWER) {
+            power = MIN_POWER;
+        }
+        motors[0].setPower(power);
+        motors[1].setPower(power);
+    }
+
+    public void loop(Gamepad gamepad) {
+        update();
+        if (gamepadStatic.isButtonPressed(gamepad, Controls.LOW)) {
+            gToPos(0)
+        } else if (GamepadStatic.isButtonPressed(gamepad, Controls.MED)) {
+            goToPos(1);
+        } else if (GamepadStatic.isButtonPressed(gamepad, Controls.HIGH)) {
+            goToPos(2);
+    }

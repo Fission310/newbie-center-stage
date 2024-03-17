38135dea63687b6b459689b0175182c6ea8e683a
TgTuro4 <tgturo4@gmail.com>
2024-03-16 22:01:41 -0400

    F,T,D,V made teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -3,17 +3,33 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.mechanisms.Arm;
+import org.firstinspires.ftc.teamcode.mechanisms.Claw;
+import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
+import org.firstinspires.ftc.teamcode.mechanisms.Slides;
+
 @TeleOp(name = "teleop")
 public class Tele extends LinearOpMode  {
     public void runOpMode() throws InterruptedException {
         // initialize the stuff
-        
+        Arm arm = new Arm(this);
+        Claw claw = new Claw(this);
+        Drivetrain drivetrain = new Drivetrain(this);
+        Slides slides = new Slides(this);
+        arm.init(hardwareMap);
+        claw.init(hardwareMap);
+        drivetrain.init(hardwareMap);
+        slides.init(hardwareMap);
+
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             // game loop
-            
             // call the loop methods for mechanisms
+            arm.loop(gamepad1);
+            claw.loop(gamepad1);
+            drivetrain.loop(gamepad1);
+            slides.loop(gamepad1);
         }
     }
 }

0f545982c0ef883afa339cdca92903466cf94466
ONLYIWIN21 <flintthecuber@gmail.com>
2024-03-16 21:22:40 -0400

    java standarts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.stuyfission.fissionlib.input.GamepadStatic;
@@ -6,40 +7,41 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.stuyfission.fissionlib.util.Mechanism;
 import com.acmerobotics.dashboard.config.Config;
-@Config
-public class Claw extends Mechanism{
-    private boolean ServoOpen = true;
 
+@Config
+public class Claw extends Mechanism {
     private Servo clawservo;
 
-    public static double Open_Pos = 0;
-    public static double Close_Pos = 0;
+    public static double openPos = 0;
+    public static double closePos = 0;
 
-    public Claw(LinearOpMode opMode) {this.opMode = opMode;}
+    public Claw(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
 
     @Override
     public void init(HardwareMap hwMap) {
         clawservo = hwMap.get(Servo.class, "clawservo");
         clawservo.setDirection(Servo.Direction.REVERSE);
-        ServoOpen();
+        open();
     }
 
-    public void ServoOpen() {
-        clawservo.setPosition(Open_Pos);
-        ServoOpen = true;
+    public void open() {
+        clawservo.setPosition(openPos);
     }
 
     public void close() {
-        clawservo.setPosition(Close_Pos);
-        ServoOpen = false;
+        clawservo.setPosition(closePos);
     }
+
     @Override
     public void loop(Gamepad gamepad) {
-        if (GamepadStatic.isButtonPressed(gamepad, Controls.CLAW_SCORE))
-        {ServoOpen();}
+        if (GamepadStatic.isButtonPressed(gamepad, Controls.CLAW_SCORE)) {
+            open();
+        }
 
-        if (GamepadStatic.isButtonPressed(gamepad, Controls.CLAW_GRAB))
-        {close();}}
-
-
+        if (GamepadStatic.isButtonPressed(gamepad, Controls.CLAW_GRAB)) {
+            close();
+        }
     }
+}
